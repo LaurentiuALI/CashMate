7480039195419dbf201288d67b1c68fd2760911a
Andoni Laurentiu <iliuta.andoni01@e-uvt.ro>
2024-03-16 13:43:25 +0200

    scaffold spring initializr

---
+++ src/main/java/com/example/CashMate/CashMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.CashMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CashMate/CashMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CashMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
